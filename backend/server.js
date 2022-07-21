45a4d80481cdd4995df137df1d39d329b656fbbc
nwaose victor <nwaosevictor1234@gmail.com>
2022-07-21 21:01:31 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 5000    
+
+app.listen(port, ()=> {
+    console.log(`app is running on http://localhost:${port} `)
+})
